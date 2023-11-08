df4aa883cd16b6bbc30143dc51e27e51d3476a7d
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-11-08 14:21:33 +0600

    Manage food route delete operation successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -38,6 +38,14 @@ async function run() {
             const cursor = foodCollection.find();
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        // Delete
+        app.delete('/food/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.deleteOne(query);
+            res.send(result);
         })
 
         // await client.db("admin").command({ ping: 1 });
